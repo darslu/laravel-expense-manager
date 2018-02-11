5282156fa6fafa74c4d8df2f1220e2778e1dc1dc
Darius <MacBook_Pro_Retina@Dariaus-MacBook-Pro.local>
2018-02-11 18:31:19 +0200

    updated and fixed income and expanse tables

---
+++ expense-manager/resources/views/admin/currencies/create.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+    <h3 class="page-title">@lang('quickadmin.currency.title')</h3>
+    {!! Form::open(['method' => 'POST', 'route' => ['admin.currencies.store']]) !!}
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            @lang('quickadmin.qa_create')
+        </div>
+        
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    {!! Form::label('title', trans('quickadmin.currency.fields.title').'', ['class' => 'control-label']) !!}
+                    {!! Form::text('title', old('title'), ['class' => 'form-control', 'placeholder' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('title'))
+                        <p class="help-block">
+                            {{ $errors->first('title') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    {!! Form::label('symbol', trans('quickadmin.currency.fields.symbol').'', ['class' => 'control-label']) !!}
+                    {!! Form::text('symbol', old('symbol'), ['class' => 'form-control', 'placeholder' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('symbol'))
+                        <p class="help-block">
+                            {{ $errors->first('symbol') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 form-group">
+                    {!! Form::label('money_format', trans('quickadmin.currency.fields.money-format').'', ['class' => 'control-label']) !!}
+                    {!! Form::text('money_format', old('money_format'), ['class' => 'form-control', 'placeholder' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('money_format'))
+                        <p class="help-block">
+                            {{ $errors->first('money_format') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+    {!! Form::submit(trans('quickadmin.qa_save'), ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+@stop
+
